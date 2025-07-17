b1a9601ad085d3e67a5a693754ddd30ebd8e9e57
replybintunet <info.replybintunet@gmail.com>
2025-07-17 19:57:49 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,19 +5,18 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Navigation } from "@/components/navigation";
 import Login from "@/pages/login";
-import Payments from "@/pages/payments";
 import Dashboard from "@/pages/dashboard";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   const [location] = useLocation();
-  
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Navigation currentPage={location} />
       <Switch>
         <Route path="/" component={Login} />
-        <Route path="/payments" component={Payments} />
+        {/* Removed the /payments route */}
         <Route path="/dashboard" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
